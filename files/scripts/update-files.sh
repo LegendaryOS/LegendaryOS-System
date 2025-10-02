58fe874159e0ad34706e3ff81340509d41d666b3
VoidArc Studio <chudybyczek2137@gmail.com>
2025-10-02 10:40:58 +0200

    Create update-files.sh

---
+++ files/scripts/update-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo chmod a+x /usr/bin/update-system
+sudo chmod a+x /usr/bin/legendary
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/update-system
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/legendary
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/Update-System-withgui.sh
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/Switch-To-Game-Mode.sh
+sudo chmod a+x /usr/share/LegendaryOS/SCRIPTS/settings-app
